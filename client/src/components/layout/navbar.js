53a656706ae559077171444349b908831c468847
Vishal <yash.codedrill@gmail.com>
2021-10-14 18:53:05 +0530

    chnges in design

--- client/src/components/layout/navbar.js
+++ client/src/components/layout/navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../../styles/Navbar.css'
+import '../../styles/components/Navbar.css'
 import {Pages} from './Pages'
 function navbar() {
   return (
@@ -11,7 +11,7 @@ function navbar() {
 <div className='nav_content'>
   {
     Pages?.map((item)=>(
-<span>{item?.path}</span>
+<span >{item?.path}</span>
     ))
   }
 
