a02c40d06d6fa68fbf442c426fbe3dde12ae0942
Yash Chandel <yash.codedrill@gmail.com>
2021-12-06 22:58:11 +0530

    add

--- client/src/components/Place/xdaXplore.js
+++ client/src/components/Place/xdaXplore.js
@@ -1,10 +1,36 @@
 import React from "react";
+import "../../styles/components/xdaXplore.css";
+import Main from "../../assets/Image1.jpeg";
+import Second from "../../assets/Image3.jpg";
+import Third from "../../assets/Image.jpg";
+
+import Fourth from "../../assets/Image2.jpg";
+import Fifth from "../../assets/Image5.jpg";
 
 function xdaXplore() {
   return (
     <div className="xplore">
       <div className="xplore_body">
         <h3>xDA Bandla</h3>
+        <div className="images">
+          <div className="main_image">
+            <img src={Main} alt="" />
+          </div>
+
+          <div className="second_column">
+            <img className="secondary_image " src={Second} alt="" />
+
+            <img className="secondary_image " src={Third} alt="" />
+          </div>
+          <div className="third_column">
+            <img className="secondary_image " src={Fourth} alt="" />
+
+            <div className="see_more" >
+                <img className='secondary_image' src={Fifth} alt=""/>
+                <button>See All Photos</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
